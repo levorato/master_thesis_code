c597fa64fbe623a7b6204081893977abe524c593
Mario Levorato <mario.levorato@terra.com.br>
2013-06-12 22:57:33 -0300

    adaptando o makefile para o Oscar.

--- mestrado/Build_Oscar/src/graph/subdir.mk
+++ mestrado/Build_Oscar/src/graph/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/graph/%.o: ../src/graph/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include/mpi -DMPIBULL_IGNORE_CXX_SEEK -DHAVE_MPI_CXX -I/opt/mpi/mpibull2-1.3.9-18.s/include -L/opt/mpi/mpibull2-1.3.9-18.s/lib/drivers/osock -L/opt/mpi/mpibull2-1.3.9-18.s/lib -lmpibinding_cxx -lmpi -lrt -ldl -L/opt/mpi/mpibull2-1.3.9-18.s/lib/pmi -lpmi -lpthread -luuid -I/usr/local/include -I$$HOME/include -O2 -g3 -Wall -c -fmessage-length=0 -pthread -m64 -Wl,-z,noexecstack -Wl,-rpath -Wl,/usr/lib64/mpich2/lib -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	icpc -I/usr/include/mpi -DMPIBULL_IGNORE_CXX_SEEK -DHAVE_MPI_CXX -I/opt/mpi/mpibull2-1.3.9-18.s/include -L/opt/mpi/mpibull2-1.3.9-18.s/lib/drivers/osock -L/opt/mpi/mpibull2-1.3.9-18.s/lib -lmpibinding_cxx -lmpi -lrt -ldl -L/opt/mpi/mpibull2-1.3.9-18.s/lib/pmi -lpmi -lpthread -luuid -I/usr/local/include -I$$HOME/include -O2 -g3 -Wall -c -fmessage-length=0 -pthread -m64 -Wl,-z,noexecstack -Wl,-rpath -Wl,/usr/lib64/mpich2/lib -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
