9fcebd3cf47ca1664fe6165a5eda4999961c281c
Mario Levorato <mario.levorato@terra.com.br>
2013-06-17 02:41:03 -0300

    Adaptando o script de leitura de resultados para capturar o cálculo de tempo global.

--- mestrado/python/process_output.py
+++ mestrado/python/process_output.py
@@ -33,6 +33,16 @@ def main(argv):
          file_list.extend(glob.glob(root + "/" + filter))
          count = len(file_list) - 1
          
+         input_file = open(root + "/result.txt", "r")
+         content = input_file.read()
+         reader = csv.reader(StringIO.StringIO(content), delimiter='\n')
+         for row in reader:
+            if ''.join(row).find("time spent:") >= 0:
+               line = ''.join(row)
+               global_time = float(line[line.find("time spent:")+11:])
+               break
+         input_file.close
+
          text_file = open(root + "/summary.txt", "w")
          filename = (root[:root.rfind("/")])
          datetime = root[root.rfind("/")+1:]
@@ -47,8 +57,10 @@ def main(argv):
          best_param = ''
 
          while count >= 0:
-            with open(file_list[count], 'r') as content_file:
-               content = content_file.read()
+            
+           content_file = open(file_list[count], 'r')
+           try:
+            content = content_file.read()
 
             reader = csv.reader(StringIO.StringIO(content), delimiter=',')
             for row in reader:
@@ -60,9 +72,9 @@ def main(argv):
                   
                   filepath = ''.join(file_list[count])
                   text_file.write(filepath[filepath.rfind("/")+1:] + ' ' + linestring + '\n')
-                  value = long(column[1])
-                  pos_value = long(column[2])
-                  neg_value = long(column[3])
+                  value = float(column[1])
+                  pos_value = float(column[2])
+                  neg_value = float(column[3])
                   K = long(column[4])
                   iteration = long(column[5]) 
                   time = float(column[6])
@@ -82,16 +94,17 @@ def main(argv):
                      best_pos_value = pos_value
                      best_neg_value = neg_value
             count = count - 1
-
+           finally:
+            content_file.close()
          text_file.close()
-         all_files_summary[filename+"/"+datetime] = str(best_value)+", "+str(pos_value)+", "+str(neg_value)+", "+str(best_K)+", "+str(iteration)+", "+str(best_time)+", "+best_param
+         all_files_summary[filename+"/"+datetime] = str(best_value)+", "+str(pos_value)+", "+str(neg_value)+", "+str(best_K)+", "+str(iteration)+", "+str(best_time)+", "+str(global_time)+", "+best_param
 
    result_file = open(folder + "/summary.txt", "w")
-   print "Filename, I(P), I(P)+, I(P)-, k, Iter, Time(s), Params"
-   result_file.write("Filename, I(P), I(P)+, I(P)-, k, Iter, Time(s), Params")
+   print "Filename, I(P), I(P)+, I(P)-, k, Iter, Local time(s), Global time(s), Params"
+   result_file.write("Filename, I(P), I(P)+, I(P)-, k, Iter, Local time(s), Global time(s), Params\n")
    for key in sorted(all_files_summary.iterkeys()):
       print "%s, %s" % (key, all_files_summary[key])
-      result_file.write("%s: %s\n" % (key, all_files_summary[key]))
+      result_file.write("%s, %s\n" % (key, all_files_summary[key]))
    result_file.close()
 
 if __name__ == "__main__":
