a6deeb23bd4e4e69eb4b2731f5b224cb2b113159
levorato <mario.levorato@gmail.com>
2013-10-02 16:18:47 -0300

    Aplicando modificacoes para funcionamento do VNS paralelo e otimizando calculo da solucao inicial do grasp. Consertando script de build do waf. Resta modificar estrutura de dados da lista de vertices da solucao inicial.

---
+++ mestrado/test-scripts-fi/s-l1-400-0.4-fiII.sh
@@ -0,0 +1,9 @@
+#INICIO DO SCRIPT
+#o script precisa entrar na pasta onde o programa esta
+export LD_LIBRARY_PATH=$HOME/lib:/opt/intel/mpi/3.2.2.006/lib64:$LD_LIBRARY_PATH
+cd /home_nfs/mlevorato/graspcc-1.0/build
+mpdboot -n 1
+mpiexec -n 1 ./graspcc -l 1 --iter=400 --alpha=0.4 --input-file-dir "/home_nfs/mlevorato/mestrado/tests/Instances/Social Media/slashdot-undirected/part0" --output-folder "./output/sdot-l1-i400-0.4-fII" --gain-function-type=4 --time-limit=3600
+mpdallexit
+#FIM DO SCRIPT
+
