c751b0afa041025c4a32d64545dfecfbe504732c
Mario Levorato <mario.levorato@terra.com.br>
2013-12-04 18:55:37 +0000

    RCCProblem.cpp alterado online com o Bitbucket - reunião 04/12/2013

--- mestrado/src/problem/RCCProblem.cpp
+++ mestrado/src/problem/RCCProblem.cpp
@@ -38,6 +38,7 @@ Imbalance RCCProblem::objectiveFunction(SignedGraph& g, const ClusterList& c) co
 	int n = g.getN();
 	scoped_array<double> internalPositiveSum(new double[nc]);
 	scoped_array<double> internalNegativeSum(new double[nc]);
+	// os valores de soma entre clusters devem compor uma matriz
 	scoped_array<double> externalPositiveSum(new double[nc]);
 	scoped_array<double> externalNegativeSum(new double[nc]);
 	std::fill_n(internalPositiveSum.get(), nc, 0.0);
@@ -66,24 +67,35 @@ Imbalance RCCProblem::objectiveFunction(SignedGraph& g, const ClusterList& c) co
 			bool sameCluster = cluster[j];
 			if(sameCluster) {
 				if(weight > 0) { // positive edge
-					internalPositiveSum[k] += weight;
+					externalPositiveSum[k][k] += weight;
 				} else { // negative edge
-					internalNegativeSum[k] += fabs(weight);
+					externalNegativeSum[k][k] += fabs(weight);
 				}
 			} else {
+			    // Find out to which cluster vertex j belongs
+				int k2 = 0;
+				for(k2 = 0; k2 < nc; k2++) {
+					cluster = c.at(k2);
+					if(cluster[j]) {
+						break;
+					}
+				}
 				if(weight > 0) { // positive edge
-					externalPositiveSum[k] += weight;
+					externalPositiveSum[k][k2] += weight;
 				} else { // negative edge
-					externalNegativeSum[k] += fabs(weight);
+					externalNegativeSum[k][k2] += fabs(weight);
 				}
 			}
 		}
 	}
 	double internalSum = 0.0, externalSum = 0.0;
 	for(int k = 0; k < nc; k++) {
-		internalSum += min(internalPositiveSum[k], internalNegativeSum[k]);
-		externalSum += min(externalPositiveSum[k], externalNegativeSum[k]);
+		internalSum += min(externalPositiveSum[k][k], externalNegativeSum[k][k]);
+		for(int k2 = 0; k2 < k; k2++) {
+			externalSum += min(externalPositiveSum[k][k2], externalNegativeSum[k][k2]);
+		}
 	}
+	
 	BOOST_LOG_TRIVIAL(trace) << "Valor calculado: " << (internalSum + externalSum) << endl;
 	return Imbalance(internalSum, externalSum);
 }
@@ -93,6 +105,7 @@ Imbalance RCCProblem::calculateDeltaObjectiveFunction(SignedGraph& g, const Clus
 		const unsigned long& k, const unsigned long& i) const {
 	double negativeSum = 0, positiveSum = 0;
 	// Not implemented.
+	// TODO marcar diferenças entre antes e depois para calcular apenas o que mudou nas matrizes
 	return Imbalance(0.0, 0.0);
 }
 
