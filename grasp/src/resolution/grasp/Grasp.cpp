2690b6d15b20127cdbc1612f8f142413c845dfa0
Mario Levorato <contato@levorato.com.br>
2015-01-01 13:52:50 -0200

    Retornando o GRASP para o critério de parada de x iterações sem melhoria.

--- grasp/src/resolution/grasp/Grasp.cpp
+++ grasp/src/resolution/grasp/Grasp.cpp
@@ -114,8 +114,7 @@ Clustering Grasp::executeGRASP(ConstructClustering &construct, VariableNeighborh
 			timeSpentOnBestSolution = timeSpentInGRASP;
 			// Registers the best result at time intervals
 			notifyNewValue(CStar, timeSpentInGRASP, totalIter);
-			// NUMBER OF ITERATIONS IS NOW FIXED !!!
-			// i = 0;
+			i = 0;
 			// TODO validar se essa saida eh valida: nao ha valor de FO menor que zero
 			if(newValue.getValue() == 0) break;
 		}
