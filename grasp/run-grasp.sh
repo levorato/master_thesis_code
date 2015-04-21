531f25daf79de9f371871bc82a6622dd3c31c392
israel <israel@israel-OptiPlex-9030-AIO.(none)>
2015-04-21 10:17:23 +0200

    Ajustando o script de build e inserindo shell script de exemplos

---
+++ grasp/run-grasp.sh
@@ -0,0 +1,3 @@
+cd build
+./graspcc -l 1 --iter=400 --alpha=1.0 --input-file "../tests/Instances/Social Media/slashdot-undirected/part0/bigger/slashdot-undirected-size4000-part0.g" --output-folder "../output/slashdot-grasp" --gain-function-type=0 --time-limit 7200 --strategy GRASP --rcc false
+
---
+++ grasp/run.sh
@@ -0,0 +1,3 @@
+cd build
+./graspcc -l 1 --iter=10 --alpha=1.0 --input-file "../tests/Instances/Social Media/slashdot-undirected/part0/bigger/slashdot-undirected-size4000-part0.g" --output-folder "../output/slashdot-ils" --gain-function-type=0 --time-limit 7200 --strategy ILS --rcc false
+
