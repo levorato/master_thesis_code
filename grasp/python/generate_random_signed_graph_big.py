e09c23a63817d8fe29eabf16dbdbbec0879c4401
Mario Levorato <contato@levorato.com.br>
2016-01-20 16:02:25 -0200

    Otimizando a geracao de grafos completamente aleatorios.

--- grasp/python/generate_random_signed_graph_big.py
+++ grasp/python/generate_random_signed_graph_big.py
@@ -41,14 +41,15 @@ class Range(object):
 
 edge = collections.namedtuple('Edge', ['x', 'y'])
 
+EPS = 0.0001
+
 
 # Returns a random edge from the graph
 def pick_random_edge(n):
-    vertex_list = range(n)
-    a = random.choice(vertex_list)
-    b = random.choice(vertex_list)
-    while (b == a):
-        b = random.choice(vertex_list)
+    a = random.randint(0, n - 1)
+    b = random.randint(0, n - 1)
+    while b == a:
+        b = random.randint(0, n - 1)
     return edge(a, b)
 
 
@@ -62,7 +63,12 @@ def generate_random_graph(n, d, dneg):
 
     m_min = int(math.ceil(d * (n * (n - 1) / 2.0)))
     mneg_min = int(math.ceil(dneg * (float(d) * (n * (n - 1) / 2.0))))
+    edge_list = []
 
+    previous_total = -1
+    percentage = 0
+    threshold = int(math.floor(mneg_min / 10.0))
+    print "Generating negative edges..."
     while mneg < mneg_min:
         e = pick_random_edge(n)
         i = e.x
@@ -73,9 +79,18 @@ def generate_random_graph(n, d, dneg):
             m += 1
             M[i,j] = -1
             M[j,i] = -1
-    print "Generated {0} negative edges.".format(str(mneg))
+            edge_list.append('({0},{1})-1\n'.format(str(i + 1), str(j + 1)))
+            percentage = int(math.ceil(100 * (float(mneg) / mneg_min)))
+            if mneg % threshold < EPS and percentage != previous_total:
+                print str(percentage) + " % ",
+                previous_total = percentage
+    print "\nGenerated {0} negative edges.".format(str(mneg))
 
+    previous_total = -1
+    percentage = 0
+    threshold = int(math.floor(m_min / 10.0))
     m = mneg
+    print "Generating positive edges..."
     while m < m_min:
         e = pick_random_edge(n)
         i = e.x
@@ -86,7 +101,12 @@ def generate_random_graph(n, d, dneg):
             m += 1
             M[i,j] = 1
             M[j,i] = 1
-    print "Generated the remaining {0} (positive) edges.".format(mpos)
+            edge_list.append('({0},{1})1\n'.format(str(i + 1), str(j + 1)))
+            percentage = int(math.ceil(100 * (float(m) / m_min)))
+            if m % threshold < EPS and percentage != previous_total:
+                print str(percentage) + " % ",
+                previous_total = percentage
+    print "\nGenerated the remaining {0} (positive) edges.".format(mpos)
 
     # Writes output file in XPRESS Mosel format (.mos)
     # --------------------------------------------------
@@ -97,23 +117,40 @@ def generate_random_graph(n, d, dneg):
     directory = "output-random-sg"
     if not os.path.exists(directory):
         os.makedirs(directory)
+    print "Saving output file..."
     with open(directory + "/" + filename, "w") as g_file:
         # file header
         g_file.write('people: {0}\r\n\r\n'.format(str(n)))
         g_file.write('VarErr: 0.5\r\n\r\n')
-        vertex_names = ""
+        vertex_names = []
+        g_file.write('Names: [')
         for x in xrange(1, n + 1):
-            vertex_names += "V" + str(x) + " "
-        g_file.write('Names: [{0}]\r\n\r\n'.format(vertex_names))
+            vertex_names.append(str(x))
+            vertex_names.append(" ")
+            if x % 20 == 0:
+                vertex_names.append('\n')
+            if len(vertex_names) >= 4096:
+                g_file.write(''.join(vertex_names))
+                vertex_names = []
+        g_file.write(''.join(vertex_names))
+        g_file.write(']\r\n')
         # graph contents
-        edge_list = ""
-        for i in xrange(n):
-            for j in xrange(n):
-                if M[i,j] != 0:
-                    edge_list += '({0},{1}){2} \r\n'.format(str(i + 1), str(j + 1), str(M[i,j]))
-        g_file.write('Mrel: [ {0}]'.format(edge_list))
+        previous_total = -1
+        percentage = 0
+        count = 0
+        list_size = len(edge_list)
+        threshold = int(math.floor(list_size / 10.0))
+        g_file.write('Mrel: [ \n')
+        for item in edge_list:
+            g_file.write(item)
+            count += 1
+            percentage = int(math.ceil(100 * (float(count) / list_size)))
+            if count % threshold < EPS and percentage != previous_total:
+                print str(percentage) + " % ",
+                previous_total = percentage
+        g_file.write(']\n')
 
-    print "Number of negative edges: {0} ({1}%), number of positive edges: {2} ({3}%), total edges: {4} ({5}%).".format(
+    print "\nNumber of negative edges: {0} ({1}%), number of positive edges: {2} ({3}%), total edges: {4} ({5}%).".format(
         str(mneg), str(100 * mneg / m), str(mpos), str(100 * mpos / m), str(m), str(100 * m / (n * (n - 1) / 2)))
     print "Output file successfully generated."
 
@@ -130,6 +167,7 @@ def main(argv):
     # measure elapsed time during graph generation
     start = time.time()
 
+    print "Will generate a completely random graph of size {0}, density = {1} and negative density = {2}".format(args.n, args.d, args.nd)
     # Call random graph generation procedure
     generate_random_graph(args.n, args.d, args.nd)
 
