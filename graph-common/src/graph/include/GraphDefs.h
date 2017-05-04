4a4f4c0643dd2d01f7c69153a4b3b12660b8aa6b
Mario Levorato <contato@levorato.com.br>
2017-05-04 02:00:41 -0300

    Incluindo varios codigos para depuracao. O problema esta na distribuicao de vertices entre processos: a distribuicao dos vertices efetivamente feita nao bate com a do splitgraph array.

---
+++ graph-common/src/graph/include/GraphDefs.h
@@ -0,0 +1,56 @@
+#ifndef GRAPH_DEFS_H_
+#define GRAPH_DEFS_H_
+
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/subgraph.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/dynamic_bitset.hpp>
+#include "../../util/serialization/dynamic_bitset.hpp"
+
+struct Edge {
+    double weight;
+    std::size_t vertex_index_t;
+    Edge() : weight(0), vertex_index_t(0) { }
+    Edge(double w) : weight(w), vertex_index_t(0) { }
+
+    friend class boost::serialization::access;
+
+	template<class Archive>
+	void serialize(Archive & ar, unsigned int file_version)
+	{
+		ar & weight;
+		ar & vertex_index_t;
+	}
+};
+struct Vertex {
+    int id;
+    std::size_t edge_index_t;
+    std::size_t vertex_rank_t;
+
+    Vertex() : id(0), edge_index_t(0) { }
+    Vertex(int w) : id(w), edge_index_t(0), vertex_rank_t(0)  { }
+
+    friend class boost::serialization::access;
+
+	template<class Archive>
+	void serialize(Archive & ar, unsigned int file_version)
+	{
+		ar & id;
+		ar & edge_index_t;
+		ar & vertex_rank_t;
+	}
+};
+
+
+enum vertex_properties_t { vertex_properties };
+enum edge_properties_t { edge_properties };
+namespace boost {
+	BOOST_INSTALL_PROPERTY(vertex, properties);
+	BOOST_INSTALL_PROPERTY(edge, properties);
+}
+
+#endif /* GRAPH_H_ */
+
